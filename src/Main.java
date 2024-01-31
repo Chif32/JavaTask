ddd136a763d7db950bf53f7fb8589b14e6e86de7
Yegor Gorbunov <yegor.gorbunov@net.ik>
2024-01-31 12:19:14 +0300

    век

--- src/Main.java
+++ src/Main.java
@@ -4,33 +4,38 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+        int num;
 
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("введите дату:");
+        num = scanner.nextInt();
 
-        Scanner input = new Scanner(System.in);
-        System.out.println("введите номер билета:");
-        int ticket = input.nextInt();
+        int century = num / 100;
 
+        switch (century) {
+            case 15:
+                System.out.println("шестнадцатый век");
+                break;
+            case 16:
+                System.out.println("семнадцатый век");
+                break;
+            case 17:
+                System.out.println("восемнадцатый век");
+                break;
+            case 18:
+                System.out.println("девятнадцатый век");
+                break;
+            case 19:
+                System.out.println("двадцатый век");
+                break;
+            case 20:
+                System.out.println("двадцать первый век");
+                break;
+            case 21:
+                System.out.println("двадцать второй век");
+                break;
+            default: System.out.println("Неправильный век");
 
-        String numberString = Integer.toString(ticket);
-        char[] array = numberString.toCharArray();
-
-        int[] intArray = new int[array.length];
-        for (int i = 0; i < array.length; i++) {
-            intArray[i] = Character.getNumericValue(array[i]);
         }
-
-        int sumFirst = intArray[0] + intArray[1] + intArray[2];
-        int sumSecond = intArray[3] + intArray[4] + intArray[5];
-
-
-        if (sumFirst == sumSecond) {
-            if (sumFirst % 2 == 0) {
-                System.out.println("Поздравляем! У вас выигрышный билет");
-            }
-        }
-        else {
-            System.out.println("Ваш билет не выиграл");
-        }
-
     }
 }
