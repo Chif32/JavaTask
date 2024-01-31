6915bbbe1475611d8f6125481e5e75c031b3e145
Yegor Gorbunov <yegor.gorbunov@net.ik>
2024-01-31 12:13:17 +0300

    билеты

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,36 @@
 package src;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int array [] = new int[50];
-        int max = array[0];
-        int min = 1;
-        int sum = array[0];
-        for (int i = 0; i < array.length; i++){
-            array[i] = (int)((Math.random()*50+0)+1);
-            if (max<array[i]){
-                max = array[i];
-            }if(min > array[i]){
-                min = array[i];
-            }
-            sum += array[i];
+
+
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("введите номер билета:");
+        int ticket = input.nextInt();
+
+
+        String numberString = Integer.toString(ticket);
+        char[] array = numberString.toCharArray();
+
+        int[] intArray = new int[array.length];
+        for (int i = 0; i < array.length; i++) {
+            intArray[i] = Character.getNumericValue(array[i]);
         }
-        sum = sum/array.length;
-        System.out.println("max число в массиве: "+max);
-        System.out.println("min число в массиве: "+min);
-        System.out.println("средн значение в массиве: "+sum);
+
+        int sumFirst = intArray[0] + intArray[1] + intArray[2];
+        int sumSecond = intArray[3] + intArray[4] + intArray[5];
+
+
+        if (sumFirst == sumSecond) {
+            if (sumFirst % 2 == 0) {
+                System.out.println("Поздравляем! У вас выигрышный билет");
+            }
+        }
+        else {
+            System.out.println("Ваш билет не выиграл");
+        }
+
     }
 }
-
