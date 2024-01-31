bd47dff2380dcd7da212524f67063d288f8600de
Yegor Gorbunov <yegor.gorbunov@net.ik>
2024-01-31 12:32:30 +0300

    чет массив

--- src/Main.java
+++ src/Main.java
@@ -1,41 +1,15 @@
 package src;
-import java.util.Scanner;
-
-public class Main {
+import java.util.stream.IntStream;
+class Main {
     public static void main(String[] args) {
 
-        int num;
+        int[] array = IntStream.rangeClosed(1, 20).toArray();
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("введите дату:");
-        num = scanner.nextInt();
 
-        int century = num / 100;
-
-        switch (century) {
-            case 15:
-                System.out.println("шестнадцатый век");
-                break;
-            case 16:
-                System.out.println("семнадцатый век");
-                break;
-            case 17:
-                System.out.println("восемнадцатый век");
-                break;
-            case 18:
-                System.out.println("девятнадцатый век");
-                break;
-            case 19:
-                System.out.println("двадцатый век");
-                break;
-            case 20:
-                System.out.println("двадцать первый век");
-                break;
-            case 21:
-                System.out.println("двадцать второй век");
-                break;
-            default: System.out.println("Неправильный век");
-
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 == 0) {
+                System.out.println(array[i]);
+            }
         }
     }
 }
